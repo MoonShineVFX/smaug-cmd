b89493906d4b90da4e7811d244f30c0f2c28483f
Noflame Lin <linjuang@gmail.com>
2023-10-05 13:34:46 +0800

    Modify error handling display

--- smaug_cmd/domain/logic.py
+++ smaug_cmd/domain/logic.py
@@ -1,4 +1,5 @@
-from typing import List, Optional, Tuple
+import logging
+from typing import List, Optional, Tuple, Callable
 from PySide6.QtCore import QObject
 
 from smaug_cmd.domain.smaug_types import Menu, MenuTree
@@ -6,27 +7,34 @@ from smaug_cmd.model import login_in as api_login
 from smaug_cmd.model import data as ds
 from smaug_cmd.ui import UploadWidget
 
+logger = logging.getLogger('smaug-cmd.domain')
+
 
 class SmaugCmdHandler(QObject):
+    
     def __init__(self, ui_widget: UploadWidget):
-        self._ui = ui_widget
-        self.__is_uploading = False
+        pass
+
+    def error_handler(self, error_msg, er_cb: Optional[Callable] = None):
+        logger.error(error_msg)
+        er_cb if er_cb(error_msg) else None
 
     def log_in(self, user_name, password) -> Tuple[int, dict]:
         return api_login(user_name, password)
 
-    def get_menus(self) -> Optional[List[Menu]]:
+    def get_menus(self, error_cb: Optional[Callable]) -> Optional[List[Menu]]:
         re = ds.get_menus()
         if str(re[0])[0] != '2':
-            self._ui.showText(re[1]["message"])
+            self.error_handler(re[1]["message"], error_cb)
             return None
         return re[1]
 
-    def get_menu_tree(self, menu_id)-> Optional[MenuTree]:
+    def get_menu_tree(self, menu_id, error_cb: Optional[Callable]=None)-> Optional[MenuTree]:
         re = ds.get_menu_tree(menu_id)
         if str(re[0])[0] != '2':
-            self._ui.showText(re[1]["message"])
+            self.error_handler(re[1]["message"], error_cb)
             return None
         return re[1]
+    
 
     
