35bc461087212c3fbbfc90be8162d1c75af38226
Noflame Lin <linjuang@gmail.com>
2023-10-07 04:42:05 +0800

    add MoonFrame for better style control

---
+++ smaug_cmd/ui/base.py
@@ -0,0 +1,10 @@
+import sys
+
+from PySide6.QtWidgets import QFrame
+
+
+class MoonFrame(QFrame):
+    def __init__(self, parent=None):
+        super(MoonFrame, self).__init__(parent)
+        if sys.platform == "win32":
+            self.setStyleSheet("MoonFrame{border: 0px;}")
