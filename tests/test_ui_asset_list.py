79fddadacbf05e55d17994959945197577daf9b3
Noflame Lin <linjuang@gmail.com>
2023-10-13 20:29:48 +0800

    rename asset list test

--- tests/test_ui_asset_list.py
+++ tests/test_ui_asset_list.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication
-from smaug_cmd.ui import UploadWidget
+from smaug_cmd.ui import AssetListDialog
 
 
 if __name__ == '__main__':
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     if not app:
         app = QApplication()
     
-    my_widget =UploadWidget()
+    my_widget =AssetListDialog()
     my_widget.show()
-    app.exec_()
+    app.exec()
