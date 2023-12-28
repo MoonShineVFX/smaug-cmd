1a786c0c7e9f519f0ee0f7982ae8351b218095b6
Noflame Lin <linjuang@gmail.com>
2023-12-29 04:30:26 +0800

    Refactor filter_by_keywords function signature
    This commit refactors the signature of the `filter_by_keywords` function in `util.py` to include type hints for the parameters and return value. This improves code readability and maintainability.

--- smaug_cmd/domain/upload_strategies/util.py
+++ smaug_cmd/domain/upload_strategies/util.py
@@ -1,4 +1,9 @@
-def filter_by_keywors(keyword_group, group_files):
+from typing import Dict, List
+
+
+def filter_by_keywors(
+    keyword_group: List[str], group_files: Dict[str, List[str]]
+) -> Dict[str, List[str]]:
     filter_group_files = {}
     for k, v in group_files.items():
         for keyeord in keyword_group:
