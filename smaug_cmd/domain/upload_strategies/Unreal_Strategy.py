36ba478e5f09b027382108112b162455c15a58b8
Noflame Lin <linjuang@gmail.com>
2024-01-25 12:10:18 +0800

    renew unreal upload representation type

--- smaug_cmd/domain/upload_strategies/Unreal_Strategy.py
+++ smaug_cmd/domain/upload_strategies/Unreal_Strategy.py
@@ -12,7 +12,6 @@ logger = logging.getLogger("smaug_cmd.domain.upload_strategy")
 
 
 class UnrealResourceUploader(UploadStrategy):
-
     def upload_previews(self, asset_template: AssetTemplate, user_id: str):
         pass
 
@@ -65,8 +64,8 @@ class UnrealResourceUploader(UploadStrategy):
     def upload_renders(self, asset_template: AssetTemplate, upload_user: str):
         """硬拿 previews 當 render 上傳"""
         # Yung Add
-        print ( '\n' )
-        print ( '>>>>> upload render' )
+        print("\n")
+        print(">>>>> upload render")
 
         previews = asset_template["previews"]
         if len(previews) == 0:
@@ -88,11 +87,10 @@ class UnrealResourceUploader(UploadStrategy):
             # print ( 'file_extension: ', file_extension )
             # print ( 'file_name: ', file_name )
             # print ( 'new_name: ', new_name )
-            print ( '>> asset_id: ', asset_id )
-            print ( 'new_name: ', new_name )
-            print ( 'render_file: ', render_file )
+            print(">> asset_id: ", asset_id)
+            print("new_name: ", new_name)
+            print("render_file: ", render_file)
             # print ( '\n' )
-
 
             # 上傳到 SSO. 這樣才能拿到 id 寫至 db
             upload_object_name = rfs.put_representation1(
@@ -128,8 +126,8 @@ class UnrealResourceUploader(UploadStrategy):
         asset_name = asset_template["name"]
 
         # yung add
-        print ( '\n' )
-        print ( '>>>>> upload models' )
+        print("\n")
+        print(">>>>> upload models")
         # print ( 'asset_id: ', asset_id )
         # print ( 'asset_name: ', asset_name )
         # print ( '>>>> asset_template:', json.dumps(asset_template, indent=4, ensure_ascii=False)  )
@@ -137,41 +135,50 @@ class UnrealResourceUploader(UploadStrategy):
         baseFolder = asset_template["basedir"]
         # print ( 'baseFolder: ', baseFolder )
 
-        subfolders = [ f.path for f in os.scandir(baseFolder) if f.is_dir() ]
+        subfolders = [f.path for f in os.scandir(baseFolder) if f.is_dir()]
         # print ( 'subfolders: ', subfolders )
-        
+
         # 篩選出UE的素材包路徑
         UEfolder = []
-        noMatchSring = [ '.smaug', '_RAR', '_Output', '_Backup', '_backup', '_Rar', '_rar' ]
+        noMatchSring = [
+            ".smaug",
+            "_RAR",
+            "_Output",
+            "_Backup",
+            "_backup",
+            "_Rar",
+            "_rar",
+        ]
         for folder in subfolders:
             if not any([x in folder for x in noMatchSring]):
                 UEfolder.append(folder)
 
-        print ( 'UEfolder: ', UEfolder )
+        print("UEfolder: ", UEfolder)
 
         if len(UEfolder) != 1:
             # 如果超過或少於一個UE素材包可以被壓縮，須進入路徑檢查
-            print ( 'errorFolder: ', baseFolder  )
+            logger.info("errorFolder: %s", baseFolder)
         else:
             # print ( 'UEfolder[0]: ', UEfolder[0] )
 
-            zip_file_name = asset_name + '_' + UEfolder[0].split('/')[-1]
-            # print ( 'zip_file_name: ', zip_file_name ) 
+            zip_file_name = asset_name + "_" + UEfolder[0].split("/")[-1]
+            # print ( 'zip_file_name: ', zip_file_name )
 
             # ziped_Model = fs.create_zip(UEfolder[0], zip_file_name)
             ziped_Model = fs.create_zip_Folder(UEfolder, zip_file_name)
             # ziped_Model = fs.create_empty_zip_Folder(UEfolder, zip_file_name)
-            print ( 'ziped_Model: ', ziped_Model )
+            print("ziped_Model: ", ziped_Model)
 
             if not os.path.exists(ziped_Model):
-                ziped_Model = ''
-                print ( 'ziped_Model is empty' )
+                ziped_Model = ""
+                print("ziped_Model is empty")
             # from smaug_cmd.adapter import fs
             # logger.info('Create "%s" Texture Zip: %s', text_key, ziped_texture)
 
-
             # 上傳至 OOS
-            upload_zip_object_name = rfs.put_representation1( asset_id, zip_file_name, ziped_Model )
+            upload_zip_object_name = rfs.put_representation1(
+                asset_id, zip_file_name, ziped_Model
+            )
 
             # 把壓縮檔移至 .smaug 下
             # moved_zip_file = fs.collect_to_smaug( asset_template["basedir"], ziped_Model )
@@ -180,9 +187,8 @@ class UnrealResourceUploader(UploadStrategy):
                 {
                     "assetId": asset_id,
                     "name": zip_file_name,
-
                     # check
-                    "type": "MODEL",
+                    "type": "GAME_ASSET",
                     "format": "UNREAL",
                     "usage": "DOWNLOAD",
                     # "fileSize": os.path.getsize(moved_zip_file),
@@ -193,50 +199,6 @@ class UnrealResourceUploader(UploadStrategy):
                 }
             )
             logger.debug("Create DB record for Asset(%s): %s", asset_id, zip_file_name)
-        
-
-                
-
-        
-
-
-        # keyword_group = ["_AvalonSource/texture", "_AvalonSource/texture_low"]
-        # group_files = util.group_files_by_directory(asset_template["textures"])
-        # filter_group_files = _filter_by_keywors(keyword_group, group_files)
-        # texture_groups = {os.path.basename(k): v for k, v in filter_group_files.items()}
-        # for text_key, files in texture_groups.items():
-        #     # make texture zip file
-        #     if len(files) == 0:
-        #         continue
-        #     zip_file_name = new_name = f"{asset_name}_{text_key}.zip"
-        #     ziped_texture = fs.create_zip(files, zip_file_name)
-        #     logger.info('Create "%s" Texture Zip: %s', text_key, ziped_texture)
-
-        #     # 上傳至 OOS
-        #     upload_zip_object_name = rfs.put_representation1(
-        #         asset_id, new_name, ziped_texture
-        #     )
-
-        #     # 把 ziped_texture 移至 .smaug 下
-        #     moved_zip_file = fs.collect_to_smaug(
-        #         asset_template["basedir"], ziped_texture
-        #     )
-
-        #     RepresentationOp.create(
-        #         {
-        #             "assetId": asset_id,
-        #             "name": zip_file_name,
-        #             "type": "TEXTURE",
-        #             "format": "IMG",
-        #             "fileSize": os.path.getsize(moved_zip_file),
-        #             "uploaderId": user_id,
-        #             "path": upload_zip_object_name,
-        #             "meta": {},
-        #         }
-        #     )
-        #     logger.debug("Create DB record for Asset(%s): %s", asset_id, zip_file_name)
-        
-
 
 
 def _filter_by_keywors(keyword_group, group_files):
